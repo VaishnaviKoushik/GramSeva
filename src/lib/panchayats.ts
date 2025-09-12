b834574bbb9462e2794f4323dad6ce573f75b371
Vaishnavi Koushik <koushikvaishu2102@gmail.com>
2025-09-12 14:05:12 +0000

    Add a the panchayatas of karanataka

--- src/lib/panchayats.ts
+++ src/lib/panchayats.ts
@@ -5,11 +5,14 @@ export type Panchayat = {
 };
 
 export const panchayats: Panchayat[] = [
-  { id: 'anandpur', name: 'Anandpur' },
-  { id: 'bijaynagar', name: 'Bijaynagar' },
-  { id: 'chandipur', name: 'Chandipur' },
-  { id: 'durgapur', name: 'Durgapur' },
-  { id: 'eklavya', name: 'Eklavya' },
-  { id: 'fatehpur', name: 'Fatehpur' },
-  { id: 'gopalganj', name: 'Gopalganj' },
+  { id: 'suggatta', name: 'Suggatta (Bangalore Urban)' },
+  { id: 'yelwal', name: 'Yelwal (Mysore)' },
+  { id: 'kakati', name: 'Kakati (Belagavi)' },
+  { id: 'munnur', name: 'Munnur (Dakshina Kannada)' },
+  { id: 'padubidri', name: 'Padubidri (Udupi)' },
+  { id: 'ayanur', name: 'Ayanur (Shivamogga)' },
+  { id: 'oorukere', name: 'Oorukere (Tumakuru)' },
+  { id: 'adakamaranahalli', name: 'Adakamaranahalli (Bangalore Urban)'},
+  { id: 'hampapura', name: 'Hampapura (Mysore)' },
+  { id: 'sankeshwar', name: 'Sankeshwar (Belagavi)' },
 ];
